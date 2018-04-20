b8676c683069bfeb49ef7cef4483c9a53e98f516
João Paulo Nobrega <joao.paulo.nobrega@avanade.com>
2018-04-20 11:20:06 -0300

    remove

--- src/config/options/test.js
+++ src/config/options/test.js
@@ -2,20 +2,24 @@ module.exports = {
     port: 8082,
     db: {
         client: 'pg',
+        seeds: {
+            directory: './seeds'
+        },
         connection: {
-            database: process.env.DATABASE_NAME || 'plataformavendas',
-            host: process.env.DATABASE_HOST || 'plataformavendas.cubskfxjipvx.sa-east-1.rds.amazonaws.com',
-            user: process.env.DATABASE_USER || 'postgres',
-            password: process.env.DATABASE_PASSWORD || 'postgres',
+            database: '',
+            host: '',
+            user: '',
+            password: '',
             charset: 'utf8'
         },
         pool: {
-            min: process.env.DATABASE_POOL_MIN || 1,
-            max: process.env.DATABASE_POOL_MAX || 1
-        }
+            min: 1,
+            max: 1
+        },
+        debug: true
     },
     productApi: {
-        uri: 'https://us-west-2.api.scaphold.io/graphql/product-api'
-        // uri: 'https://us-west-2.api.scaphold.io/graphql/product-api'
+        //uri: 'http://localhost:3030/api/v1/graphql'
+        uri: ''
     }
 }
